0f4108f12c8f8e3c49594c672185a5f376cae7d8
Nekralius <81307194+Necralius@users.noreply.github.com>
2024-08-12 18:28:17 -0300

    Project Commit -> Main Systems Development 6.7 (Victor Paulo - NDC)
    - New character controller

---
+++ Dead Echo Project/Assets/DeadEchoProject/Scripts/SurvivalSystem/SurvivalAspects.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SurvivalAspects : MonoBehaviour
+{
+    [SerializeField] private float _health = 100;
+    //private float 
+
+    [SerializeField] private SurvivalAspect _hunger  = new SurvivalAspect();
+    [SerializeField] private SurvivalAspect _thirst  = new SurvivalAspect();
+    [SerializeField] private SurvivalAspect _stamina = new SurvivalAspect();
+
+    [SerializeField] private bool isRunning  = false;
+    [SerializeField] private bool isWalking  = false;
+    [SerializeField] private bool isCrouched = false;
+
+    private void Update()
+    {
+        if (_hunger is not null) 
+            _hunger.Update(ref _health);
+
+        if (_thirst is not null) 
+            _thirst.Update(ref _health);
+
+        if (_stamina is not null)
+            _stamina.Update(ref _health);
+    }
+}
+
+[Serializable]
+public class SurvivalAspect
+{
+    [Range(0, 400)] public float Value              = 0;
+    [Range(0, 400)] public float MaxValue           = 0;
+    [Range(0, 20)]  public float HealthLossByTime   = 1;
+    [Range(0, 20)]  public float AspectLossByTime   = 1;
+
+    [Range(0, 40)]  public float LossLimit = 0;
+
+    public bool HealthLossActive { get => Value <= LossLimit; }
+
+    /// <summary>
+    /// This method updates this
+    /// </summary>
+    /// <param name="currentHealth"></param>
+    /// <param name="effectors"></param>
+    public void Update(ref float currentHealth)
+    {
+        Value -= AspectLossByTime / 1000;
+        if (HealthLossActive) 
+            currentHealth -= HealthLossByTime / 1000;
+    }
+}
